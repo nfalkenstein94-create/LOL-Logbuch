36f38b3213f746a2f5acf36612868c0e11fc81a8
nfalkenstein94-create <n.falkenstein94@gmail.com>
2026-09-10 19:44:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'arena-logbuch-v3';
+const CACHE = 'arena-logbuch-v4';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './firebase-config.js',
   './icon-180.png', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 
